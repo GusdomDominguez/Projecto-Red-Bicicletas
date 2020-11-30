d3a621eb480f6e1bffd3717d8b2b51ae50c4f94f
Agustin Dominguez <gusdomdominguez@gmail.com>
2020-11-30 14:59:23 -0300

    Conexion con base de datos y creado modelo Bicicleta

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -1,36 +1,35 @@
-var Bicicleta = function (id, color, modelo, ubicacion) {
-  this.id = id;
-  this.color = color;
-  this.modelo = modelo;
-  this.ubicacion = ubicacion;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var bicicletaSchema = new Schema({
+  code: Number,
+  color: String,
+  modelo: String,
+  ubicacion: {
+    type: [Number],
+    index: { type: "2dsphere", sparse: true },
+  },
+});
+
+bicicletaSchema.methods.toString = function () {
+  return `code: ${this.code} | color: ${this.color}`;
 };
 
-Bicicleta.prototype.toString = function () {
-  return `id: ${this.id} | color: ${this.color}`;
+bicicletaSchema.statics.allBicis = function (cb) {
+  return this.find({}, cb);
 };
 
-Bicicleta.allBicis = [];
-Bicicleta.add = function (aBici) {
-  Bicicleta.allBicis.push(aBici);
+bicicletaSchema.statics.createInstance = function (
+  code,
+  color,
+  modelo,
+  ubicacion
+) {
+  return new this({
+    code: code,
+    color: color,
+    modelo: modelo,
+    ubicacion: ubicacion,
+  });
 };
-Bicicleta.findById = function (aBiciId) {
-  var aBici = Bicicleta.allBicis.find((x) => x.id == aBiciId);
-  if (aBici) return aBici;
-  else throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
-};
-Bicicleta.removeById = function (aBiciId) {
-  for (var i = 0; i < Bicicleta.allBicis.length; i++) {
-    if (Bicicleta.allBicis[i].id == aBiciId) {
-      Bicicleta.allBicis.splice(i, 1);
-      break;
-    }
-  }
-};
-
-/* var a = new Bicicleta(1, "rojo", "urbana", [-32.974189, -68.70766]);
-var b = new Bicicleta(2, "blanca", "urbana", [-32.979189, -68.71966]);
-
-Bicicleta.add(a);
-Bicicleta.add(b); */
-
-module.exports = Bicicleta;
+module.exports = mongoose.model("Bicicleta", bicicletaSchema);
--- spec/support/models/bicicleta_test_spec.js
+++ spec/support/models/bicicleta_test_spec.js
@@ -1,6 +1,59 @@
+var mongoose = require("mongoose");
 var Bicicleta = require("../../../models/bicicleta");
 
-beforeEach(() => {
+describe("Testing Bicicletas", function () {
+  beforeEach(function (done) {
+    var mongoDB = "mongodb://localhost/testdb";
+    mongoose.connect(mongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    mongoose.set("useCreateIndex", true);
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "conection error"));
+    db.once("open", function () {
+      console.log("We are connected to test database.");
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    Bicicleta.deleteMany({}, function (err, success) {
+      if (err) console.log(err);
+      done();
+    });
+  });
+
+  describe("Bicicleta.createInstance", () => {
+    it("crea una instancia de Bicicleta", () => {
+      var bici = Bicicleta.createInstance(1, "verde", "urbana", [
+        -32.974189,
+        -68.70766,
+      ]);
+
+      expect(bici.code).toBe(1);
+      expect(bici.color).toBe("verde");
+      expect(bici.modelo).toBe("urbana");
+      expect(bici.ubicacion[0]).toEqual(-32.974189);
+      expect(bici.ubicacion[1]).toEqual(-68.70766);
+    });
+  });
+
+  describe("Bicicleta.allBicis", () => {
+    it("comienza vacia", (done) => {
+      Bicicleta.allBicis(function (err, bicis) {
+        if (err) {
+          console.log(err);
+        } else {
+          expect(bicis.length).toBe(0);
+          done();
+        }
+      });
+    });
+  });
+});
+
+/* beforeEach(() => {
   Bicicleta.allBicis = [];
 });
 
@@ -53,3 +106,4 @@ describe("Bicicleta.remove", () => {
     expect(Bicicleta.allBicis.length).toBe(0);
   });
 });
+ */
